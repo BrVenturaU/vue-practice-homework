c10a30ac59843232a89615183725d41672fce4e0
BrVenturaU <brandonventura16@gmail.com>
2021-01-31 23:23:40 -0600

    Agrega componentes de VUE.
    -Agrega los listados de materias, ciclos y facultades.
    -Agrega metodo de creacion de inscripciones.

---
+++ scripts.js
@@ -0,0 +1,22 @@
+var inscripcionModel = {alumno:"", docente:"", ciclo:"Ciclo I", materia:"Materia 1", facultad:"Ciencia y Tecnología"};
+const app = new Vue({
+    el: "#app",
+    data:{
+        ciclos: ["Ciclo I", "Ciclo II", "Ciclo III", "Ciclo IV", "Ciclo V", "Ciclo VI", "Ciclo VII", "Ciclo VIII", "Ciclo IX", "Ciclo X"],
+        materias: ["Materia 1", "Materia 2", "Materia 3", "Materia 4", "Materia 5"],
+        facultades: ["Ciencia y Tecnología", "Ciencias de la Salud", "Ciencias y Humanidades", "Ingeniería y Arquitectura"],
+        inscripciones: [],
+        inscripcion: inscripcionModel
+    },
+    methods: {
+        inscribir(){
+            this.inscripciones.push({
+                alumno: this.inscripcion.alumno,
+                docente:this.inscripcion.docente,
+                ciclo:this.inscripcion.ciclo,
+                materia:this.inscripcion.materia,
+                facultad:this.inscripcion.facultad
+            });
+        }
+    },
+});
